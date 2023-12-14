8c5bc76a61952d808e83bdd02f582675b548b583
PreetiSwami <preetiswami0308@gmail.com>
2023-12-14 18:41:13 +0530

    leaflet
    geo location

--- src/app/crop-statistics/crop-statistics.component.ts
+++ src/app/crop-statistics/crop-statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js/auto'
 
 @Component({
   selector: 'app-crop-statistics',
@@ -10,6 +11,47 @@ export class CropStatisticsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    this.createBarChart();
   }
+
+  createBarChart(): void {
+    const canvas = document.getElementById('barChart') as HTMLCanvasElement;
+    const ctx:any = canvas.getContext('2d');
+    
+    const data = {
+      labels: ['Label 1', 'Label 2', 'Label 3'], // Replace with your labels
+      datasets: [{
+        label: 'Bar Chart',
+        data: [10, 20, 30], // Replace with your data values
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)', // Example color
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)', // Example border color
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+        ],
+        borderWidth: 1
+      }]
+    };
+  
+    const config:any = {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    };
+  
+    new Chart(ctx, config);
+  }
+  
 
 }
--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-location',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
+  private map: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.initMap();
+  }
+
+  private initMap(): void {
+
+    const latitude = 51.505; // Replace with your latitude
+    const longitude = -0.09; // Replace with your longitude
+    const zoomLevel = 13; // Replace with your desired zoom level
+
+    this.map = L.map('map').setView([latitude, longitude], zoomLevel);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    const marker = L.marker([latitude, longitude]).addTo(this.map);
   }
 
 }
